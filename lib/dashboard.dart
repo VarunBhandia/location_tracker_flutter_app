7dfe3cb95820ff47b070fe96a0f5dfeba35f7f17
varunbhandia <varunbhandia@gmail.com>
2020-04-20 22:54:48 +0530

    Added conditions

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -19,7 +19,7 @@ class _DashboardPage extends State<DashboardPage> {
   static const ms = const Duration(milliseconds: 1);
   double latitude;
   double longitude;
-
+  bool showAlert = false;
   _getCurrentLocation() {
     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
@@ -33,12 +33,14 @@ class _DashboardPage extends State<DashboardPage> {
 
       print("LAT final: ${latitude}, LNG final: ${longitude}");
       print("LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}");
-
-      double var1 = (_currentPosition.latitude - latitude)*(_currentPosition.latitude - latitude);
-      double var2 = (_currentPosition.latitude - latitude)*(_currentPosition.latitude - latitude);
-      double var3 = var1 + var2;
-      double dist = pow(var3,0.5);
+      double dist = getDistanceFromLatLonInKm(_currentPosition.latitude ,latitude,_currentPosition.longitude ,longitude);
       print(dist);
+
+      if(dist > 6000){
+        setState(() {
+          showAlert = true;
+        });
+      }
     }).catchError((e) {
       print(e);
     });
@@ -50,9 +52,22 @@ class _DashboardPage extends State<DashboardPage> {
     Location location = await helper.queryLocation(rowId);
     latitude = location.latitude;
     longitude = location.longitude;
-
-
   }
+
+  double getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    double R = 6371; // Radius of the earth in km
+    double dLat = deg2rad(lat2-lat1);  // deg2rad below
+    double dLon = deg2rad(lon2-lon1);
+    double a =  sin(dLat/2) * sin(dLat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon/2) * sin(dLon/2);
+    double c = 2 * atan2(sqrt(a), sqrt(1-a));
+    double d = R * c; // Distance in km
+    return d;
+  }
+
+  double deg2rad(deg) {
+    return deg * (3.14/180);
+  }
+
 
   @override
   void initState() {
@@ -68,15 +83,20 @@ class _DashboardPage extends State<DashboardPage> {
       appBar: AppBar(
         title: Text("Dashboard"),
       ),
-      body: Container(
-          margin: const EdgeInsets.all(20.0),
-          color: Colors.redAccent,
-          width: 800.0,
-          height: 100.0,
-          child: Center(
-            child: Text('Go back in your circle please !!!',style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 30),),
-          )
+      body:AnimatedOpacity(
+        opacity: showAlert ? 1 : 0,
+        duration: Duration(milliseconds: 200),
+        child: Container(
+            margin: const EdgeInsets.all(20.0),
+            color: Colors.redAccent,
+            width: 800.0,
+            height: 100.0,
+            child: Center(
+              child: Text('Go back in your circle please !!!',style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 30),),
+            )
+        ),
       ),
+
     );
   }
 
