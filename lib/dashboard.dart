9df58a2e12f832a67739d83397458e1029169c38
varunbhandia <varunbhandia@gmail.com>
2020-04-21 12:24:44 +0530

    Merge and removed unwanted codes

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -35,10 +35,9 @@ class _DashboardPage extends State<DashboardPage> {
       print("LAT final: ${latitude}, LNG final: ${longitude}");
       print("LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}");
       distanceInMeters = await Geolocator().distanceBetween(latitude, longitude, _currentPosition.latitude, _currentPosition.longitude);
-//      double dist = getDistanceFromLatLonInKm(_currentPosition.latitude ,latitude,_currentPosition.longitude ,longitude);
       print(distanceInMeters);
 
-      if(distanceInMeters > 100){
+      if(distanceInMeters > 1){
         setState(() {
           showAlert = true;
         });
@@ -59,21 +58,6 @@ class _DashboardPage extends State<DashboardPage> {
     latitude = location.latitude;
     longitude = location.longitude;
   }
-
-  double getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    double R = 6371; // Radius of the earth in km
-    double dLat = deg2rad(lat2-lat1);  // deg2rad below
-    double dLon = deg2rad(lon2-lon1);
-    double a =  sin(dLat/2) * sin(dLat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon/2) * sin(dLon/2);
-    double c = 2 * atan2(sqrt(a), sqrt(1-a));
-    double d = R * c; // Distance in km
-    return d;
-  }
-
-  double deg2rad(deg) {
-    return deg * (3.14/180);
-  }
-
 
   @override
   void initState() {
