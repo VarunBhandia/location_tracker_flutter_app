07a642475efd6a7d2c7e551d17a34fd3fd8ee492
varunbhandia <varunbhandia@gmail.com>
2020-04-21 12:58:41 +0530

    Added notification

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,9 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
 import 'database_helper.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -12,6 +12,7 @@ class DashboardPage extends StatefulWidget {
 
 
 class _DashboardPage extends State<DashboardPage> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   Position _currentPosition;
 
@@ -61,10 +62,20 @@ class _DashboardPage extends State<DashboardPage> {
 
   @override
   void initState() {
+
+    super.initState();
     _read();
     const oneSec = const Duration(seconds:10);
     new Timer.periodic(oneSec, (Timer t) => _getCurrentLocation());
-    super.initState();
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    // If you have skipped STEP 3 then change app_icon to @mipmap/ic_launcher
+    var initializationSettingsAndroid =
+    new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,onSelectNotification: onSelectNotification);
   }
 
   @override
@@ -76,18 +87,52 @@ class _DashboardPage extends State<DashboardPage> {
       body:AnimatedOpacity(
         opacity: showAlert ? 1 : 0,
         duration: Duration(milliseconds: 200),
-        child: Container(
-            margin: const EdgeInsets.all(20.0),
-            color: Colors.redAccent,
-            width: 800.0,
-            height: 100.0,
-            child: Center(
-              child: Text('Go back in your circle please !!!',style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 30),),
-            )
-        ),
-      ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              color: Colors.redAccent,
+              width: 800.0,
+              height: 100.0,
+              child: Center(
+                child: Text('Go back in your circle please !!!',style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 30),),
+              )
+            ),
+            new RaisedButton(
+              onPressed: _showNotificationWithDefaultSound,
+              child: new Text('Show Notification Without Sound'),
+            ),
+          ],
+        )
 
+
+    ),
     );
   }
-
+  Future onSelectNotification(String payload) async {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return new AlertDialog(
+          title: Text("PayLoad"),
+          content: Text("Payload : $payload"),
+        );
+      },
+    );
+  }
+  Future _showNotificationWithDefaultSound() async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
 }
